5a0847939a776551390744646a1cc985cc27fab5
awarnofullstack <awarnofullstack@gmail.com>
2024-01-18 22:24:50 +0530

    push

--- routes/counselor/dashboard/index.js
+++ routes/counselor/dashboard/index.js
@@ -44,7 +44,7 @@ router.get("/payments", async (req, res) => {
 router.get("/counselor-student", async (req, res) => {
 
     const totalStudents = await StudentInCounselor.find({ counselor: new ObjectId(req.user._id) }).countDocuments()
-    const totalCounselors = await  CounselorMember({ counselor: new ObjectId(req.user._id) }).countDocuments();
+    const totalCounselors = await CounselorMember.find({ counselor: new ObjectId(req.user._id) }).countDocuments();
     const response = responseJson(true, { totalStudents, totalCounselors }, '', 200);
     return res.status(200).json(response);
 });
